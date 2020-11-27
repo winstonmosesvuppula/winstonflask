4b1576bf7840ad6a077fbef9912bd85c2f9920f9
winstonmosesvuppula <75005838+winstonmosesvuppula@users.noreply.github.com>
2020-11-27 19:44:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,13 +7,5 @@ def home():
     return render_template('home.html')
 
 
-@app.route("/test1")
-def test1():
-    return render_template('test1.html')
-
-@app.route("/dashboard")
-def dashboard():
-    return render_template('/dashboard/index.html')
-
 if __name__ == '__main__':
     app.run()
